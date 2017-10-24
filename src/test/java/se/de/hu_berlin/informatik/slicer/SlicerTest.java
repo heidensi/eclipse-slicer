651b85e0f46634fbef7a028495daf82030c60afd
Simon Heiden <heiden@informatik.hu-berlin.de>
2017-10-24 16:00:08 +0200

    basic project structure dummy

---
+++ src/test/java/se/de/hu_berlin/informatik/slicer/SlicerTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package se.de.hu_berlin.informatik.slicer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ * @author Simon
+ *
+ */
+public class SlicerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link se.de.hu_berlin.informatik.slicer.Slicer#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
