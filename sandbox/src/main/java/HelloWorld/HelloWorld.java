c44b1e08f1ed7b9aa0044312266993f91c68cff1
Simon Heiden <heiden@informatik.hu-berlin.de>
2017-11-10 13:13:11 +0100

    ...and the HelloWorld program (should be the one in the jar)

---
+++ sandbox/src/main/java/HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+public class HelloWorld {
+	
+	private static int x = 2;
+	private static int y = 3;
+
+	public static void main(String[] args) {
+		
+		int z = 2;
+		
+		System.out.println("Hello World!" + x + " " + z);
+		
+		x += y;
+	}
+
+}
